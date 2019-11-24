ff954923b06194f8f33094259aca75b0c4411020
Alanfxx <j.alands2@gmail.com>
2019-11-24 12:19:11 -0300

    implementação do finByDepartment

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -87,5 +88,39 @@ public class SellerDaoJDBC implements SellerDao {
 		dep.setId(rs.getInt("DepartmentId"));
 		dep.setName(rs.getString("DepName"));
 		return dep;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"WHERE DepartmentId = ? "
+					+"ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			List<Seller> list = new ArrayList<>();
+			Department dep;
+			if(rs.next()) {
+				dep = instantiateDepartment(rs);
+				rs.beforeFirst();
+				while (rs.next()) {
+					Seller obj = instantiateSeller(rs, dep);
+					list.add(obj);
+				}
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 }
